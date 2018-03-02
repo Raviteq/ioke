c2763d97496ae6cbe3aca82a44e229f45a457a19
Manuel Astudillo <manuel@optimalbits.com>
2018-03-02 19:47:49 +0100

    implemented a local file cache using kashmir

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+var gm = require("gm");
+var express = require("express");
+var path = require("path");
+var Promise = require("bluebird");
+var urlJoin = require("url-join");
+var kashmir = require('kashmir');
 
-var gm = require('gm');
-var express = require('express');
-var path = require('path');
-var Promise = require('bluebird');
-var urlJoin = require('url-join');
-var request = require('request');
+var middleware = function(rootPath, backend, translator, headers, cacheOpts) {
+  var cache;
+  if(cacheOpts){
+    cache = new kashmir.Cache(cacheOpts);
+  }
 
-var middleware = function(rootPath, backend, translator, headers){
-  return function (req, res, next) {
-
+  return function(req, res, next) {
     //
     // Get transform object from query string.
     //
@@ -18,76 +21,118 @@ var middleware = function(rootPath, backend, translator, headers){
     // Fetch backend.
     //
     try {
-      translator = translator || function(rootPath, req){
-        return validUrl.isUri(rootPath) ? urlJoin(rootPath, req.path) : path.join(rootPath, req.path);
-      }
+      translator =
+        translator ||
+        function(rootPath, req) {
+          return validUrl.isUri(rootPath)
+            ? urlJoin(rootPath, req.path)
+            : path.join(rootPath, req.path);
+        };
 
-      Promise.resolve(translator(rootPath, req)).then(function(srcPath){
-        var src = backend.fetch(srcPath)
+      Promise.resolve(translator(rootPath, req))
+        .then(function(srcPath) {
+          var opts = typeof opts == "string" ? {url: opts} : srcPath;
 
-        src.stream.on('error', function(err){
-          if(err.code == 'ENOENT'){
-            //res.status(404).end();
-            next();
-          }else{
-            console.error(err)
-            res.status(500).end(err);
-          }
-        });
-
-        //
-        // Transform if needed.
-        // TODO: Use thread to perform the transform without idling the server.
-        //
-        if (transform) {
-          if(headers){
-            res.set(Object.assign({
-              'Content-Type': src.mimeType
-            }, headers));
-          }
-
-          if (isCrop(transform)) {
-            if (transform.r) {
-              var img = gm(src.stream).size({ bufferStream: true }, function (err, size) {
-                if (err) {
-                  res.status(500).send(err);
-                  return;
-                }
-                var w = size.width;
-                var h = size.height;
-                img.crop(
-                  (transform.w * w) / 100,
-                  (transform.h * h) / 100,
-                  (transform.x * w) / 100,
-                  (transform.y * h) / 100).stream().pipe(res);
-              });
-            } else {
-              gm(src.stream).crop(transform.w, transform.h, transform.x, transform.y).stream().pipe(res);
-            }
-
-          } else if (isScale(transform)) {
-            gm(src.stream).resize(transform.w, transform.h, transform.r ? '%' : '').stream().pipe(res);
-          }
-        } else {
-          src.stream.on('response', function(res){
-            if(headers){
-              for(header in headers){
-                var lowerCaseheader = header.toLowerCase();
-                if(typeof headers[header] === 'undefined'){
-                  delete res.headers[lowerCaseheader];
-                }else{
-                  res.headers[lowerCaseheader] = headers[header];
-                }
+          (cache ? cache.get(opts.url) : Promise.resolve() ).then( function(cached){
+            console.log(cached)
+            if(cached){
+              return {
+                stream: cached.stream,
+                size: cached.size,
+                mimeType: cached.meta.mimeType
               }
-              console.log(headers, res.headers);
+            }else {
+              return backend.fetch(srcPath, cache).then(function(src) {
+                if(cache){
+                  cache.set(opts.url, src.stream, src.size, {mimeType: src.mimeType})
+                }
+                return src;
+              });
             }
-          }).pipe(res);
-        }
-      }).catch(function(err) {
-        console.error(err);
-        res.status(404).end();
-      });
-    }catch(err){
+          }).then( function(src){
+
+            src.stream.on("error", function(err) {
+              if (err.code == "ENOENT") {
+                next();
+              } else {
+                console.error(err);
+                res.status(500).end(err);
+              }
+            });
+
+            //
+            // Transform if needed.
+            //
+            if (transform) {
+              if (headers) {
+                res.set(
+                  Object.assign(
+                    {
+                      "Content-Type": src.mimeType
+                    },
+                    headers
+                  )
+                );
+              }
+
+              if (isCrop(transform)) {
+                if (transform.r) {
+                  var img = gm(src.stream).size(
+                    { bufferStream: true },
+                    function(err, size) {
+                      if (err) {
+                        res.status(500).send(err);
+                        return;
+                      }
+                      var w = size.width;
+                      var h = size.height;
+                      img
+                        .crop(
+                          transform.w * w / 100,
+                          transform.h * h / 100,
+                          transform.x * w / 100,
+                          transform.y * h / 100
+                        )
+                        .stream()
+                        .pipe(res);
+                    }
+                  );
+                } else {
+                  gm(src.stream)
+                    .crop(transform.w, transform.h, transform.x, transform.y)
+                    .stream()
+                    .pipe(res);
+                }
+              } else if (isScale(transform)) {
+                gm(src.stream)
+                  .resize(transform.w, transform.h, transform.r ? "%" : "")
+                  .stream()
+                  .pipe(res);
+              }
+            } else {
+              src.stream
+                .on("response", function(res) {
+                  if (headers) {
+                    for (header in headers) {
+                      var lowerCaseheader = header.toLowerCase();
+                      if (typeof headers[header] === "undefined") {
+                        delete res.headers[lowerCaseheader];
+                      } else {
+                        res.headers[lowerCaseheader] = headers[header];
+                      }
+                    }
+                    console.log(headers, res.headers);
+                  }
+                })
+                .pipe(res);
+            }
+          });
+        })
+        .catch(function(err) {
+          console.error(err);
+          res.status(404).end();
+        });
+    } catch (err) {
       console.error(err);
       res.status(404).end();
       return;
@@ -97,69 +142,107 @@ var middleware = function(rootPath, backend, translator, headers){
       res.status(404).end();
     });
     */
-  }
-}
+  };
+};
 
-module.exports = function (rootPath, backend) {
-
+module.exports = function(rootPath, backend) {
   var app = express.Router();
 
-  app.get('*', middleware(rootPath, backend));
+  app.get("*", middleware(rootPath, backend));
 
   function transformEtag(transform) {
     if (transform) {
-      var props = ['w', 'h', 'x', 'y'];
-      return props.reduce(function (prev, cur) {
+      var props = ["w", "h", "x", "y"];
+      return props.reduce(function(prev, cur) {
         if (transform[cur]) {
           return prev + transform[cur];
         }
         return prev;
-      }, '');
+      }, "");
     } else {
-      return '';
+      return "";
     }
   }
 
   return app;
-}
+};
 
 function isCrop(transform) {
-  return transform &&
-    (transform.x || transform.y) &&
-    (transform.w || transform.h)
+  return (
+    transform && (transform.x || transform.y) && (transform.w || transform.h)
+  );
 }
 
 function isScale(transform) {
-  return (transform &&
-    transform.w ||
-    transform.h);
+  return (transform && transform.w) || transform.h;
 }
 
 //
 // Simple Backend
 //
-var mime = require('mime-types');
-var fs = require('fs');
+var mime = require("mime-types");
+var fs = require("fs");
 Promise.promisifyAll(fs);
 
-var etag = require('etag');
-var validUrl = require('valid-url');
+var etag = require("etag");
+var validUrl = require("valid-url");
 
-var Backend = function(){
-  this.fetch = function (srcPath) {
-    var mimeType = mime.lookup(srcPath);
-    if(validUrl.isUri(srcPath)){
-      return {
-        mimeType: mimeType,
-        stream: request.get(srcPath)
-      }
-    }else{
-      return {
-        mimeType: mime.lookup(srcPath),
-        stream: fs.createReadStream(srcPath),
-      }
+var Backend = function() {
+  this.fetch = function(opts) {
+    var mimeType = mime.lookup(opts.url);
+    if (validUrl.isUri(opts.url)) {
+      return getHttpStream(opts);
+    } else {
+      return getFileStream(opts);
     }
-  }
+  };
+};
+
+var http = require('http');
+var https = require('https');
+var url = require('url');
+function getHttpStream(opts){
+  var parsedUrl = url.parse(opts.url);
+  opts = Object.assign({
+   method: 'GET',
+   protocol: parsedUrl.protocol,
+   hostname: parsedUrl.hostname,
+   path: parsedUrl.path,
+   port: parsedUrl.port ? parsedUrl.port : parsedUrl.protocol === 'https:' ? 443 : 80,
+  }, opts);
+
+  var protocol = opts.protocol == 'https:' ? https : http;
+
+  return new Promise(function(resolve, reject) {
+    var req = protocol.request(opts,  function(res) {
+      console.log(`STATUS: ${res.statusCode}`);
+    
+      resolve({
+        stream: res,
+        size: parseInt(res.headers["content-length"]),
+        mimeType: res.headers["content-type"] || mime.lookup(opts.url)
+      });
+    });
+
+    req.on('error', reject);
+    req.end();
+  });
+}
+
+function getFileStream(opts) {
+  return new Promise(function(resolve, reject) {
+    fs.stat(opts.url, function(err, stats) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({
+          mimeType: mime.lookup(opts.url),
+          stream: fs.createReadStream(opts.url),
+          size: stats.size
+        });
+      }
+    });
+  });
 }
 
 module.exports.Backend = Backend;
